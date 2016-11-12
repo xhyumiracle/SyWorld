080febf16927db860417584ab6f716b4febe8900
xhyumiracle <xhyumiracle@gmail.com>
2016-11-12 01:42:12 -0400

    Updated SySocket, added ReliableUdpSocket, planning to make a connection-free reliable layer.

--- SyWorldNew/SySocket.py
+++ SyWorldNew/SySocket.py
@@ -1,13 +1,40 @@
 import socket
 from SyConfig import debug_out, debug_con, sleep_time, destination_ip_port, my_address_port,\
     SOCKET_RCV_BUF_SIZE, SOCKET_SND_BUF_SIZE, online
+
+class ReliableUdpSocket():
+    def __init__(self, netType, protocolType):
+        self.sock = socket.socket(netType, protocolType)
+
+    def setsockopt(self, config, attr, value):
+        return self.sock.setsockopt(config, attr, value)
+
+    def bind(self, tupleAddressPort):
+        return self.sock.bind(tupleAddressPort)
+
+    def setblocking(self, isBlock):
+        return self.sock.setblocking(isBlock)
+
+    def close(self):
+        return self.sock.close()
+
+    def sendto(self, data, flags=None, *args, **kwargs): # real signature unknown; NOTE: unreliably restored from __doc__
+        return self.sock.sendto(data, flags, *args, **kwargs)
+
+    def recvfrom(self, buffersize, flags=None): # real signature unknown; restored from __doc__
+        if not flags:
+            return self.sock.recvfrom(buffersize)
+        else:
+            return self.sock.recvfrom(buffersize, flags)
+
 class SySocket():
     def __init__(self):
         if debug_con == 0:
             self.sock = self.socket_init(my_address_port)
 
     def socket_init(self, address_port, blocking=True):
-        soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        soc = ReliableUdpSocket(socket.AF_INET, socket.SOCK_DGRAM)
         soc.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, SOCKET_SND_BUF_SIZE)
         soc.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, SOCKET_RCV_BUF_SIZE)
         soc.bind(address_port)
@@ -20,7 +47,6 @@ class SySocket():
     def socket_close(self):
         # connection.close()
         self.sock.close()
-
     #
     #               TYPE    TYPE-mean       STR-mean
     # MouseEnter    set     setmousepos     x,y
